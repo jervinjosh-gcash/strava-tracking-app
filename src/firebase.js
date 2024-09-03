9c88b4cde9d58d0f0907d6fd1e7fa8264d8266e3
jervinjosh68 <jervin.josh.valencia@eee.upd.edu.ph>
2024-09-03 21:56:49 +0800

    hid my access keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,22 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBZ3xhamnePu2uMpACUigOYIdR4KAKdsKs",
-  authDomain: "strava-app-db.firebaseapp.com",
-  projectId: "strava-app-db",
-  storageBucket: "strava-app-db.appspot.com",
-  messagingSenderId: "495495487439",
-  appId: "1:495495487439:web:e8f9bdae0c49f766c1bcba"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
