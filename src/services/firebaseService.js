8d5b8992c8fefaceb9a51fd41f72881d74fa7143
Jervin Josh Valencia <jervinjosh.valencia@GFI6760.local>
2025-01-14 15:36:09 +0800

    added console log to check if user works

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -10,7 +10,7 @@ export async function fetchStravaCredentials() {
   if (!user) {
     throw new Error("User is not authenticated.");
   }
-
+  console.log("user aquired");
   const userRef = doc(db, "users", user.uid);
   try {
     const userDoc = await getDoc(userRef);
